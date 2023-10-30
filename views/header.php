c0d11cec326ae3540329e73570fda2d25bdf2a6b
mrsaif888 <usaif453@gmail.com>
2023-10-30 23:02:28 +0500

    Connection with database

--- views/header.php
+++ views/header.php
@@ -1,3 +1,4 @@
+<?php include_once './database/connection.php'; ?>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1">
     <link href="style.css" rel="stylesheet" type="text/css">
