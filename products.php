1e359eba6bf9200e1d38ddd31a7491811341de6a
mrsaif888 <usaif453@gmail.com>
2023-11-18 15:36:28 +0500

    Implement pagination functionality in products.php to display a list of products.

--- products.php
+++ products.php
@@ -2,56 +2,77 @@
 session_start();
 require_once './database/connection.php';
 
+// Pagination settings
+$productsPerPage = 4;
+$page = isset($_GET['p']) ? (int) $_GET['p'] : 1;
+$offset = ($page - 1) * $productsPerPage;
+
+// Fetch products from the database
+$sql = "SELECT * FROM products LIMIT $offset, $productsPerPage";
+$result = mysqli_query($connection, $sql);
+
 ?>
+
 <html>
 
 <head>
-    
     <title>Products</title>
     <?php include './views/header.php'; ?>
 </head>
 
 <body>
-<?php include './views/nav.php'; ?>
+    <?php include './views/nav.php'; ?>
     <main>
         <div class="products content-wrapper">
             <h1>Products</h1>
-            <p>6 Products</p>
-            <div class="products-wrapper">
-                <a href="product.php" class="product">
-                    <img src="imgs/wallet.jpg" width="200" height="200" alt="Wallet">
-                    <span class="name">Wallet</span>
-                    <span class="price">
-                        $14.99 <span class="rrp">$19.99</span>
-                    </span>
-                </a>
-                <a href="product.php" class="product">
-                    <img src="imgs/headphones.jpeg" width="200" height="200" alt="Headphones">
-                    <span class="name">Headphones</span>
-                    <span class="price">
-                        $19.99 </span>
-                </a>
-                <a href="product.php" class="product">
-                    <img src="imgs/watch.jpg" width="200" height="200" alt="Smart Watch">
-                    <span class="name">Smart Watch</span>
-                    <span class="price">
-                        $29.99 </span>
-                </a>
-                <a href="product.php" class="product">
-                    <img src="imgs/camera.jpg" width="200" height="200" alt="Digital Camera">
-                    <span class="name">Digital Camera</span>
-                    <span class="price">
-                        $69.99 </span>
-                </a>
-            </div>
-            <div class="buttons">
-                <a href="products.php&amp;p=2">Next</a>
-            </div>
-        </div>
+              <div class="products-wrapper">
+            <?php
+            // Display products
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<a href="product.php" class="product">';
+                echo '<img src="imgs/'.$row['img'].'" width="200" height="200" alt="'.$row['name'].'">';
+                echo '<span class="name">'.$row['name'].'</span>';
+                echo '<span class="price">$'.$row['price'].'</span>';
+                echo '</a>';
+            }
 
+            // Display pagination links
+            $sql = 'SELECT COUNT(*) AS total FROM products';
+            $result = mysqli_query($connection, $sql);
+            $row = mysqli_fetch_assoc($result);
+            $totalProducts = $row['total'];
+            $totalPages = ceil($totalProducts / $productsPerPage);
+
+         // Display pagination links
+            echo '</div><div class="pagination">';
+
+            // Previous link
+            if ($page > 1) {
+                echo '<a href="products.php?p='.($page - 1).'">Previous</a>';
+            }
+
+            // Page numbers
+            echo '<span class="page">Page '.$page.' of '.$totalPages.'</span>';
+            for ($i = 1; $i <= $totalPages; ++$i) {
+                if ($i === $page) {
+                    echo '<span class="current">'.$i.'</span>';
+                } else {
+                    echo '<a href="products.php?p='.$i.'">'.$i.'</a>';
+                }
+            }
+
+            // Next link
+            if ($page < $totalPages) {
+                echo '<a href="products.php?p='.($page + 1).'">Next</a>';
+            }
+
+            echo '</div>';
+            ?>
+
+        
+        </div>
     </main>
     <?php include './views/footer.php'; ?>
-
 </body>
 
 </html>
